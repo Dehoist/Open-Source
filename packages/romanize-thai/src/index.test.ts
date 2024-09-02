a166ffd9219b2599ef9ca65f42a2fc189e65c834
Slowlife <slowlife1165@gmail.com>
2024-09-02 13:39:06 +0700

    feat: add thai language (#3)
    * feat: add thai language
    
    * chore: add some tests
    
    * feat: improve romanization for Thai language
    
    * chore: change test a little
    
    ---------
    
    Co-authored-by: Bas950 <me@bas950.com>

--- packages/romanize-thai/src/index.test.ts
+++ packages/romanize-thai/src/index.test.ts
@@ -1,11 +1,24 @@
 import { describe, it } from 'vitest'
+import { THAI_REGEX, successTests } from './mocks/testData.js'
+
+import { romanize } from './index.js'
 
 describe('romanize-thai', () => {
-  it('should romanize thai text', () => {
-    // test code here
+  it('should romanize thai text', ({ expect }) => {
+    for (const [input, output] of successTests) {
+      const result = romanize(input)
+      expect(result).not.toMatch(THAI_REGEX)
+      expect(result).toBe(output)
+    }
   })
 
-  it('should not romanize non-thai text', () => {
-    // test code here
+  it('should not romanize non-thai text', ({ expect }) => {
+    expect(romanize('hello')).toBe('hello')
+    expect(romanize('123')).toBe('123')
+    expect(romanize('!@#$%^&*()')).toBe('!@#$%^&*()')
+    //* Shouldn't romanize different languages (e.g. Japanese, Chinese, Korean)
+    expect(romanize('こんにちは')).toBe('こんにちは')
+    expect(romanize('你好')).toBe('你好')
+    expect(romanize('안녕하세요')).toBe('안녕하세요')
   })
 })
